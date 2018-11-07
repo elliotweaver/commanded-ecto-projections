5b957dc4c4623912535dd7c87b449c2e07c4d4eb
elliotweaver <elliot.weaver@gmail.com>
2018-11-07 07:43:56 -0800

    bump

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Commanded.Projections.Ecto.Mixfile do
   defp deps do
     [
       {:commanded, ">= 0.12.0", runtime: false},
-      {:ecto, "~> 2.2", runtime: false},
+      {:ecto, "~> 3.0", runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:postgrex, "~> 0.13", only: :test},
       {:mix_test_watch, "~> 0.9", only: :dev, runtime: false}
